9d67f9b7839efc87ebd973763c516de1de7b0b88
DouglasVilasBoas <douglas.silvabatista1013.dsb@gmail.com>
2024-05-20 23:16:48 -0300

    error

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,17 +3,27 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         
         const { name, email, password } = request.body;
 
-        if (!name) {
-            throw new AppError("Nome é obrigatório!");
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]) //e para evitar criar um email duplicado
+
+        if (checkUserExist) {
+            throw new AppError("Este e-mail já esta em uso.");
         }
-
-
-        response.status(201).json({ name, email, password });
+       
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?), ",
+            [name, email, password]
+        
+        );
+       
+       
+        return response.status(201).json();
 
     }
+
 }
 module.exports = UsersController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,24 +2,13 @@ const { Router } = require("express");
 const  UsersController = require("../controllers/usersController");
 
 
-
 const usersRoutes = Router();
-
-function myMiddleware(request, response, next) {
-    console.log("Você passou pelo Middleware!")
-    
-    if (!request.body.isAdmin) {
-        return response.json({ message: "user unauthorized" });
-    }
-    
-    next();
-}
 
 
 const usersController = new UsersController(); //criando um nova estancia de uma classe  
 
 
-usersRoutes.post("/",myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 
 
