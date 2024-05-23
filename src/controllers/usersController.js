9e469c9898d688e5921c570bf4c2f7ae07c84af6
DouglasVilasBoas <douglas.silvabatista1013.dsb@gmail.com>
2024-05-22 22:01:21 -0300

    debugado o error

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const {hash} = require ("bcryptjs")  //importando bibliotecas para o projeto
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite")
 
@@ -9,14 +9,21 @@ class UsersController {
 
         const database = await sqliteConnection();
         const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]) //e para evitar criar um email duplicado
-
-        if (checkUserExist) {
+        const checkUserExistName = await database.get("SELECT * FROM users WHERE name = (?)", [name])
+        
+        if (checkUserExist) { 
             throw new AppError("Este e-mail já esta em uso.");
         }
-       
+        
+        if (checkUserExistName) { 
+            throw new AppError("Este nome já esta em uso.");
+        }
+        
+        const hashedPassword = await hash(password, 8); // tem ser assíncrona por tem que esperar fazer a criptografia 
+
         await database.run(
-            "INSERT INTO users (name, email, password) VALUES (?, ?, ?), ",
-            [name, email, password]
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?) ",
+            [name, email, hashedPassword]
         
         );
        
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 require("express-async-errors");
+const database = require("./database/sqlite");
 const  migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");  // importou express
 const routes = require("./routes");
+
+migrationsRun(); 
 
 const app = express(); // iniciou o express
 app.use(express.json());
 
 app.use(routes);
 
-migrationsRun();  //rodando o banco de dados
-
-//status de error 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
@@ -21,6 +21,7 @@ app.use((error, request, response, next) => {
     }
 
     console.error(error);
+    
     return response.status(500).json({
         status: "error",
         message: "Internal server error"
@@ -32,4 +33,4 @@ const PORT = 3333; //criou um constância e a porta que vai ser usado
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); //listen e tipo escutando quando ela executar vai rodar a mensagem do console.log (terminal)
 
 
-//PAREI que erro e esse 
+//esta funcionando hehehe
